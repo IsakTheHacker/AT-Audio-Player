1f177b6508c7962b1a1c14926a192af7d5838ba1
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-07 02:18:07 +0100

    Setup pygame and create a simple testscript.

--- Python AT Audio Player/Python_AT_Audio_Player.py
+++ Python AT Audio Player/Python_AT_Audio_Player.py
@@ -1,1 +1,10 @@
 
+#Imports
+import pygame										#Import pygame
+
+pygame.mixer.init()									#Initalize mixer
+pygame.mixer.music.load("song.mp3")					#Load song
+pygame.mixer.music.play()							#Play song!
+
+while True:
+	pass
