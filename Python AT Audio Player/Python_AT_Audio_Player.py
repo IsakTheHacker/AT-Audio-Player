e693fac327902926346fdae3079be482420e8be8
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-07 03:11:30 +0100

    Add ability to stop and reload songs.

--- Python AT Audio Player/Python_AT_Audio_Player.py
+++ Python AT Audio Player/Python_AT_Audio_Player.py
@@ -13,6 +13,7 @@ print(welcome_message)
 for var in welcome_message:
 	print("-", end="", flush=True)
 	time.sleep(0.05)
+print()
 
 pygame.mixer.init()									#Initalize mixer
 pygame.mixer.music.load("song.mp3")					#Load song
@@ -22,5 +23,17 @@ while True:
 	char = msvcrt.getwch()
 	if char == "p" and pygame.mixer.music.get_busy():
 		pygame.mixer.music.pause()
+		print("Paused!")
 	elif char == "p" and not pygame.mixer.music.get_busy():
 		pygame.mixer.music.unpause()
+		print("Unpaused!")
+
+	if char == "s":
+		pygame.mixer.music.stop()
+		print("Stopped!")
+
+	if char == "l":
+		new_song = input("Type the new song here: ")
+		pygame.mixer.music.load(new_song)
+		pygame.mixer.music.play()
+		print("New song loaded!")
