7b775483e44342a18211050e066a7f256212ffff
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-07 03:04:41 +0100

    Add ability to pause and unpause.

--- Python AT Audio Player/Python_AT_Audio_Player.py
+++ Python AT Audio Player/Python_AT_Audio_Player.py
@@ -1,10 +1,26 @@
 
 #Imports
-import pygame										#Import pygame
+import pygame
+import sys
+import time
+import msvcrt
+
+#Variables
+welcome_message = "Welcome to Python AT Audio Player!"
+
+#Welcome the user
+print(welcome_message)
+for var in welcome_message:
+	print("-", end="", flush=True)
+	time.sleep(0.05)
 
 pygame.mixer.init()									#Initalize mixer
 pygame.mixer.music.load("song.mp3")					#Load song
 pygame.mixer.music.play()							#Play song!
 
 while True:
-	pass
+	char = msvcrt.getwch()
+	if char == "p" and pygame.mixer.music.get_busy():
+		pygame.mixer.music.pause()
+	elif char == "p" and not pygame.mixer.music.get_busy():
+		pygame.mixer.music.unpause()
