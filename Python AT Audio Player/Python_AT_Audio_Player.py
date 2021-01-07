55f75cae20a7185621997b222fec5bb178a801a2
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-07 05:12:14 +0100

    Add support for folder playlists.

--- Python AT Audio Player/Python_AT_Audio_Player.py
+++ Python AT Audio Player/Python_AT_Audio_Player.py
@@ -47,12 +47,26 @@ while True:
 	if char == "i":
 		playlist = input("Type the playlist here: ")
 		if os.path.exists(playlist):
-			with open(playlist) as file:
-				data = json.load(file)
-			pygame.mixer.music.load(data["songs"][0])
-			for entry in data["songs"][1:len(data["songs"])]:
-				pygame.mixer.music.queue(entry)
-			pygame.mixer.music.play()
+			if os.path.isfile(playlist):
+				with open(playlist) as file:
+					data = json.load(file)
+				pygame.mixer.music.load(data["songs"][0])
+				for entry in data["songs"][1:len(data["songs"])]:
+					pygame.mixer.music.queue(entry)
+				pygame.mixer.music.play()
+			elif os.path.isdir(playlist):
+				files = []
+				for entry in os.scandir(playlist):
+					if (entry.is_file()):
+						if entry.name.endswith(".mp3"):
+							files.append(entry.name)
+				if len(files) > 0:
+					pygame.mixer.music.load(playlist + "/" + files[0])
+					for file in files[1:len(files)]:
+						pygame.mixer.music.queue(playlist + "/" + file)
+					pygame.mixer.music.play()
+				else:
+					print("Directory does not contain any mp3-files")
 		else:
 			print("Playlist doesn't exist.")
 
