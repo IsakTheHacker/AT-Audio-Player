7a54244389b94fa5f681471d994305143c3cac9b
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-09 20:32:34 +0100

    Print length in now playing menu.

--- Python AT Audio Player/Python_AT_Audio_Player.py
+++ Python AT Audio Player/Python_AT_Audio_Player.py
@@ -151,6 +151,7 @@ while shouldRun:
 		print("Name: {}".format(nowPlaying.name))
 		print("Path: {}".format(nowPlaying.path))
 		print("Paused: {}".format(nowPlaying.paused))
+		print("Length: {}".format(nowPlaying.length))
 		print("Position (ms): {}".format(pygame.mixer.music.get_pos()))
 
 	if char == "h":
